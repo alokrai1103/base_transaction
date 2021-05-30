fd20463db74537b4e8dba7fdba2227362609f4f7
alokrai1103 <83207087+alokrai1103@users.noreply.github.com>
2021-05-30 22:14:41 +0530

    Merged steps 6, 6.1 and 7 into 3

--- 03 update Member Membership Opportunity (excluding AMEX and Online).sql
+++ 03 update Member Membership Opportunity (excluding AMEX and Online).sql
@@ -1,10 +1,23 @@
+
+
 UPDATE
-    cef_prod.source_stg_base_transaction B LEFT JOIN
+    cef_prod.source_stg_base_transaction B 
+    LEFT JOIN
     Netsuite.MEMBERSHIP M
         ON M.ORIGINAL_LINKED_TRANSACTION_ID = B.fulfill_id 
+        LEFT JOIN
+    Netsuite.DEPARTMENTS D
+        ON M.Department_Id = D.DEPARTMENT_ID 
+        LEFT JOIN
+    Netsuite.LECLUB_TIER_LIST L
+        ON L.LIST_ID = M.MEMBERSHIP_TIER_ID
 SET
-      membership_id = M.MEMBERSHIP_ID
-    , renewal_opportunity_id = M.RENEWAL_OPPORTUNITY_ID
-    , actual_customer_id = M.MEMBER_NAME_ID  
+      B.membership_id = M.MEMBERSHIP_ID
+    , B.renewal_opportunity_id = M.RENEWAL_OPPORTUNITY_ID
+    , B.actual_customer_id = M.MEMBER_NAME_ID  
+	, B.membership_department = D.NAME
+    , B.membership_lifecycle = M.LIFECYCLE_YEAR
+    , B.membership_tier = L.LIST_ITEM_NAME
+    , B.membership_parentId = M.PARENT_MEMBER_ID
 WHERE
     M.MEMBERSHIP_ID IS NOT NULL

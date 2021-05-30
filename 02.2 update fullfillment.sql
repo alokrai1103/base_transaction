e5876b84927af781fb3e870adeb7795674981d9e
alokrai1103 <83207087+alokrai1103@users.noreply.github.com>
2021-05-30 19:48:59 +0530

    Updated fulfillment id

--- 02.2 update fullfillment.sql
+++ 02.2 update fullfillment.sql
@@ -1,29 +1,69 @@
-WITH
-    fulfill_temp AS
-    (
-        SELECT 
-              T3.Transaction_ID AS fulfill_id
-            , T1.Transaction_ID
-        FROM
-            cef_prod.source_stg_base_transaction B INNER JOIN
-            Netsuite.TRANSACTIONS T1
-                ON B.transaction_id = T1.Transaction_ID INNER JOIN
-            Netsuite.TRANSACTIONS T3
-                ON T1.CREATED_FROM_ID = T3.CREATED_FROM_ID INNER JOIN
-            Netsuite.TRANSACTION_LINES TL
-                ON TL.TRANSACTION_ID = T3.TRANSACTION_ID INNER JOIN
-            Netsuite.ITEMS IT
-                ON TL.ITEM_ID = IT.item_id
-        WHERE
-            T1.CREATED_FROM_ID IS NOT NULL
-            AND T3.CREATED_FROM_ID IS NOT NULL
-            AND T3.TRANSACTION_TYPE = 'Item Fulfillment'
-            AND IT.ITEM_TYPE_ID IN (1, 2, 3, 4)
-    )
+---- WHEN CREATED_FROM_ID IS NOT NULL for INVOICE & CASH SALE
 
 UPDATE
-    cef_prod.source_stg_base_transaction B INNER JOIN
-    fulfill_temp A
-        ON A.Transaction_ID = B.Transaction_ID
+    Base_Transaction B 
+        INNER JOIN
+    TRANSACTIONS T1
+        ON B.sale_order_id = T1.CREATED_FROM_ID
 SET
-    B.fulfill_id = A.fulfill_id
+    B.fulfill_id = T1.Transaction_ID 
+
+WHERE
+            B.sale_order_id IS NOT NULL
+            AND T1.CREATED_FROM_ID IS NOT NULL
+            AND T1.TRANSACTION_TYPE = 'Item Fulfillment'
+
+
+
+---- WHEN BOTH CREATED_FROM_ID & DUPLICATE_CREATED_FROM_ID IS NULL for INVOICE & CASH SALE
+
+	UPDATE
+    Base_Transaction B 
+        INNER JOIN
+    (
+        SELECT
+              B.TRANSACTION_ID
+            , t_ful.TRANSACTION_ID as fulfill_id
+        FROM
+			Base_Transaction B 
+			    INNER JOIN
+			TRANSACTIONS T 
+				ON T.duplicate_created_from_id = B.transaction_id 
+				AND T.TRANSACTION_TYPE = 'Sales Order' 
+				INNER JOIN
+            TRANSACTIONS t_ful 
+				ON t_ful.CREATED_FROM_ID = T.TRANSACTION_ID 
+				AND t_ful.TRANSACTION_TYPE = 'Item Fulfillment' 
+				
+        WHERE
+            T.TRANSACTION_TYPE = 'Sales Order'
+            AND t_ful.TRANSACTION_TYPE = 'Item Fulfillment'
+    ) A
+        ON A.TRANSACTION_ID = B.transaction_id
+            
+	SET
+      B.fulfill_id = A.fulfill_id 
+
+	--- WHEN BOTH DUPLICATE_CREATED_FROM_ID IS NOT NULL for INVOICE & CASH SALE
+
+	UPDATE
+    Base_Transaction B 
+        INNER JOIN
+    (
+        SELECT
+              B.TRANSACTION_ID
+			, t_ful.TRANSACTION_ID as fullfill_id
+           
+        FROM  
+			Base_Transaction B 
+			    INNER JOIN
+            TRANSACTIONS t_ful 
+				ON t_ful.TRANSACTION_TYPE = 'Item Fulfillment' 
+				AND t_ful.CREATED_FROM_ID = B.duplicate_created_from_id 
+        WHERE
+            B.duplicate_created_from_id is not null
+            
+    ) A
+        ON A.TRANSACTION_ID = B.transaction_id
+	SET
+		B.fulfill_id = A.fulfill_id     
